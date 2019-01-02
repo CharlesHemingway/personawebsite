af8a8e9bb2478c2d5e46d48d73db826c1f63afbb
Ossiel Romero <ossiel@riseup.net>
2019-01-02 15:15:36 -0600

    initial commit
    personal website

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('C:/Users/Ossie/Desktop/TrabajoOssiel/gatsby/ossiel.com/node_modules/gatsby-plugin-google-analytics/gatsby-browser'),
+      options: {"plugins":[],"trackingId":"UA-98616028-1"},
+    },{
+      plugin: require('C:/Users/Ossie/Desktop/TrabajoOssiel/gatsby/ossiel.com/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
